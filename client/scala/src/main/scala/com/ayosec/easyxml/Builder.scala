57ec0c65c71a88031d7c0dcd346b209cebd3a33d
Ayose <ayosec@gmail.com>
2011-12-19 01:59:03 +0000

    Add EasyXML package
    Easy XML generation based on javax.xml.stream

---
+++ client/scala/src/main/scala/com/ayosec/easyxml/Builder.scala
@@ -0,0 +1,95 @@
+package com.ayosec.easyxml
+
+import javax.xml.stream._
+import javax.xml.stream.events.{StartElement, EndElement, XMLEvent}
+import javax.xml.namespace.QName
+
+object Builder {
+
+  def apply(pathName: String, root: String) = new Builder(new java.io.FileOutputStream(pathName), root)
+
+  def apply(file: java.io.File, root: String) = new Builder(new java.io.FileOutputStream(file), root)
+
+  def apply(stream: java.io.OutputStream, root: String) = new Builder(stream, root)
+
+}
+
+package subcontent {
+
+  class TagWithContent(val tag: Tag, val content: (Builder) => Unit)
+
+  class Tag(val open: StartElement, val close: EndElement) {
+    def ~(content: (Builder) => Unit) = new TagWithContent(this, content)
+  }
+
+  class StandaloneTag(val tag: String)
+
+}
+
+class Builder(
+  val output: java.io.OutputStream,
+  val rootTagName: String) {
+
+  private val factory = XMLEventFactory.newInstance
+
+  private val writer = {
+    val writer = XMLOutputFactory.newInstance.createXMLEventWriter(output)
+
+    writer.add(factory.createStartDocument)
+    writer.add(factory.createStartElement("", "", rootTagName))
+    writer
+  }
+
+  def close() {
+    writer.add(factory.createEndElement("", "", rootTagName))
+    writer.add(factory.createEndDocument)
+    output.close()
+  }
+
+  /*
+   * Helpers to predefine content
+   */
+
+  import subcontent._
+
+  def tag(tagName: String) = {
+    new Tag(factory.createStartElement("", "", tagName), factory.createEndElement("", "", tagName))
+  }
+
+  def cdata(content: String) = factory.createCData(content)
+
+  def attribute(name: String, value: String) = factory.createAttribute(new QName(name), value)
+  def attribute(name: String) = factory.createAttribute(new QName(name), _: String)
+
+  /*
+   * “Appenders”
+   */
+
+  def <<(anyText: String) = {
+    writer.add(factory.createCharacters(anyText))
+    this
+  }
+
+  def <<(tag: Tag) = {
+    writer.add(tag.open)
+    this
+  }
+
+  def <<(event: XMLEvent) = {
+    writer.add(event)
+    this
+  }
+
+  def <<(tagWithContent: TagWithContent) = {
+    writer.add(tagWithContent.tag.open)
+    tagWithContent.content(this)
+    writer.add(tagWithContent.tag.close)
+    this
+  }
+
+  def <<!(cdata: String) = {
+    writer.add(factory.createCData(cdata))
+    this
+  }
+
+}
---
+++ client/scala/src/test/scala/easyxml/EasyXMLDSL.scala
@@ -0,0 +1,98 @@
+package easyxml
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import com.ayosec.easyxml.Builder
+
+class EasyXMLDSL extends FlatSpec with ShouldMatchers {
+
+  val xmlHead = """<?xml version="1.0" encoding="UTF-8"?>"""
+
+  it should "writes to an ByteArrayOutputStream" in {
+    val stream = new java.io.ByteArrayOutputStream
+    val builder = Builder(stream, "root")
+
+    val tag = builder.tag("foo")
+    builder << tag << "bar" << tag.close
+    builder.close
+
+    stream.toString should be (xmlHead + """<root><foo>bar</foo></root>""")
+  }
+
+  it should "writes to a file" in {
+    val tempFile = java.io.File.createTempFile("test-easyxml", ".xml")
+
+    // Write some XML content
+    val builder = Builder(tempFile, "root")
+    val tag = builder.tag("foo")
+    builder << tag << "bar" << tag.close
+    builder.close
+
+    // Check the result
+    val expectedString = xmlHead + """<root><foo>bar</foo></root>"""
+    val fileReader = new java.io.FileReader(tempFile)
+    val fileContent = new Array[Char](expectedString.size)
+
+    fileReader.read(fileContent, 0, expectedString.size)
+
+    fileReader.read should be (-1) // should be EOF
+    fileContent.mkString should be (expectedString)
+  }
+
+  it should "adds attributes to the last open tag" in {
+    val stream = new java.io.ByteArrayOutputStream
+    val builder = Builder(stream, "root")
+
+    val tagFoo = builder.tag("foo")
+    val tagBar = builder.tag("bar")
+    builder <<
+      tagFoo <<
+        builder.attribute("one", "1") <<
+        builder.attribute("two", "2") <<
+        "bar" <<
+        tagBar <<
+          builder.attribute("three", "3") <<
+        tagBar.close <<
+      tagFoo.close
+    builder.close
+
+    stream.toString should be (xmlHead + """<root><foo one="1" two="2">bar<bar three="3"></bar></foo></root>""")
+  }
+
+  it should "use attributes as a partially-applied functions" in {
+
+    val stream = new java.io.ByteArrayOutputStream
+    val builder = Builder(stream, "root")
+
+    val tag = builder.tag("foo")
+    val attr = builder.attribute("one")
+    builder << tag << attr("1") << "bar" << tag.close
+    builder.close
+
+    stream.toString should be (xmlHead + """<root><foo one="1">bar</foo></root>""")
+  }
+
+  it should "accepts a block in tags" in {
+
+    val stream = new java.io.ByteArrayOutputStream
+    val builder = Builder(stream, "root")
+
+    val tagFoo = builder.tag("foo")
+    val tagBar = builder.tag("bar")
+    builder << (tagFoo ~ { _ << (tagBar ~ { _ << "text" }) })
+    builder.close
+
+    stream.toString should be (xmlHead + """<root><foo><bar>text</bar></foo></root>""")
+  }
+
+  it should "add CDATA sections using <<!" in {
+    val stream = new java.io.ByteArrayOutputStream
+    val builder = Builder(stream, "root")
+    builder << (builder.tag("foo") ~ { _ <<! "raw text" })
+    builder << builder.cdata("a < b")
+    builder.close
+
+    stream.toString should be (xmlHead + """<root><foo><![CDATA[raw text]]></foo><![CDATA[a < b]]></root>""")
+  }
+
+}
